91ddf7c4becfff3d0a56c3bec29174bb1ce8102a
github username <github email address>
2018-06-19 11:34:18 -0400

    roll back migrations and create new migration

--- db/migrate/20180619143854_create_portfolios_table.rb
+++ db/migrate/20180619143854_create_portfolios_table.rb
@@ -1,8 +1,8 @@
 class CreatePortfoliosTable < ActiveRecord::Migration[5.0]
   def change
     create_table :portfolios do |t|
-      t.string :user
-      t.string :company
+      t.integer :user_id
+      t.integer :company_id
     end
   end
 end
