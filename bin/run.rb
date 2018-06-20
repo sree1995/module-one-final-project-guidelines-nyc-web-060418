f190ff22d86a2b91098fdaf6d350c6ca9733e6b1
github username <github email address>
2018-06-20 16:58:23 -0400

    add more method to run

--- bin/run.rb
+++ bin/run.rb
@@ -11,6 +11,7 @@ def welcome
 end
 
 def help_before_user_created
+  puts " "
   puts "login                                 to log into the application"
   puts "create account                        creates a new username"
   puts "list                                  lists companies available to add to your portfolio"
@@ -22,7 +23,7 @@ def help_after_user_created
   puts "list                                  lists companies available to add to your portfolio"
   puts "list my portfolio                     lists your portfolio companies"
   puts "portfolio mood                        lists your portfolio companies and their market mood to"
-  puts "add company                           adds a company to your portfolio"
+  puts "add company                           adds a company from the list to your portfolio"
   puts "create company                        adds a company to the list and adds the company to your portfolio"
   puts "delete company                        removes company from your portfolio list"
   puts "help                                  lists commands aviable"
@@ -38,7 +39,7 @@ def app_flow_before_user_created
 
   case user_command
   when "help"
-    help_before_user_created
+    app_flow_before_user_created
   when "login"
     login_process
   when "create account"
@@ -92,8 +93,80 @@ def login_process
 end
 
 def app_flow_after_user_created(login_name)
-  help_after_user_created
+
   logged_in_user = User.find_user(login_name)
+
+  app_flow_after_commands
+
+end
+
+def app_flow_after_commands
+  help_after_user_created
+  puts "Enter your command"
+  user_command = gets.downcase.chomp
+
+  case user_command
+  when "help"
+    app_flow_after_commands
+  when "logout"
+    abort("Thank You. Hope You made $$$$$")
+  when "list"
+    companies_in_list
+  when "list my portfolio"
+    logged_in_user.list_portfolio_companies
+  when "portfolio mood"
+    logged_in_user.list_portfolio_sentiments
+  when "add company"
+    add_a_company_process
+  when "create company"
+    create_a_company_process
+  when "delete company"
+    delete_a_company_process
+  end
+end
+
+def delete_a_company_process
+  puts "Enter company name"
+  given_company_name = gets.chomp
+
+  if logged_in_user.companies.find_by(name:given_company_name) != nil
+    logged_in_user.delete_a_company_from_users_list(given_company_name)
+    app_flow_after_commands
+  else logged_in_user.companies.find_by(name:given_company_name) == nil
+    puts "Company doesn't exist in Your portfolio"
+    puts "Check your list"
+    companies_in_list
+    app_flow_after_commands
+  end
+end
+
+def create_a_company_process
+  puts "Enter company name"
+  given_company_name = gets.chomp
+  puts "Enter company ticker_symbol"
+  given_ticker_symbol = gets.upcase.chomp
+
+  if logged_in_user.companies.find_by(name:given_company_name) == nil
+    logged_in_user.create_and_add_new_company_to_user(given_company_name, given_ticker_symbol)
+    app_flow_after_commands
+  else
+    puts "Company Already Exists!"
+    companies_in_list
+    app_flow_after_commands
+  end
+end
+
+def add_a_company_process
+  puts "Enter company name"
+  given_company_name = gets.chomp
+  if logged_in_user.companies.find_by(name:given_company_name) == nil
+    logged_in_user.add_a_company_from_the_list(given_company_name)
+    app_flow_after_commands
+  else
+    puts "Wrong Company Name, check for case sestivity"
+    companies_in_list
+    add_a_company_process
+  end
 end
 
 app_flow_before_user_created
