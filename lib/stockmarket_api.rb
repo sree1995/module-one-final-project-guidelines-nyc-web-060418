573e9e5d93ce95b477b5d84527cd018d2d094d0d
github username <github email address>
2018-06-21 11:32:28 -0400

    stockmarket api methods working

--- lib/stockmarket_api.rb
+++ lib/stockmarket_api.rb
@@ -3,19 +3,37 @@ require 'json'
 
 class StockDataAPI
 
-  alphavantage_web_address  = "https://www.alphavantage.co/query?function="
+  AV_WEB_ADDRESS  = "https://www.alphavantage.co/"
 
 
+  ## load yml file
+  key = YAML.load_file('alphavantage_apikey.yml')
+  API_KEY = key['CONSUMER_KEY']
 
-  def data_request(complete_web_address)
-    json_data = RestClient.get(complete_web_address)
+
+  def data_request(api_link)
+    json_data = RestClient.get(api_link)
     data_hash = JSON.parse(json_data)
   end
 
-  def complete_web_address
-    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo"
+  def api_parameters(ticker_symbol)
+    function = "TIME_SERIES_DAILY_ADJUSTED"
+    symbol = ticker_symbol
+    apikey = API_KEY
+    api_link = AV_WEB_ADDRESS + "query?" + "function=#{function}&" + "symbol=#{symbol}&" + "datatype=json&" + "apikey=#{apikey}"
+    data_request(api_link)
   end
 
+  def relavent_data(ticker_symbol)
+    data_hash = api_parameters(ticker_symbol)
 
+    time_series = data_hash["Time Series (Daily)"].collect do |day|
+      day
+    end
+    time_series[0][1]
+  end
 
+  def self.call(ticker_symbol)
+    StockDataAPI.new.relavent_data(ticker_symbol)
+  end
 end
